ed41831ea33271527d947cd3395584e07f1d2eb3
Rajesh Dulal <146376153+georajesh@users.noreply.github.com>
2024-01-21 18:53:53 -0500

    Update myStory.js

--- story/myStory.js
+++ story/myStory.js
@@ -7,10 +7,6 @@ function initMap() {
     const iconURL = { url: 'https://georajesh.github.io/geom99lab1/story/airport.png', 
                     // This marker is 20 pixels wide by 32 pixels high.
                     size: new google.maps.Size(20, 32),
-                    // The origin for this image is (0, 0).
-                    origin: new google.maps.Point(0, 0),
-                    // The anchor for this image is the base of the flagpole at (0, 32).
-                    anchor: new google.maps.Point(0, 32),
                     }
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 3,
