3570f0d607315651ae7c9240b42fb4f91b29f574
Rajesh Dulal <146376153+georajesh@users.noreply.github.com>
2024-01-22 03:30:51 -0500

    Zoom Changed

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 5,
     center: { lat: 45.62240711525139, lng: -82.09748924346194 },
   });
   // Create an array of alphabetical characters used to label the markers.
