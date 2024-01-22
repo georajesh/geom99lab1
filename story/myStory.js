73bc69fcd734d3e6f105d53756bb78e84152caf9
Rajesh Dulal <146376153+georajesh@users.noreply.github.com>
2024-01-21 19:16:19 -0500

    Update myStory.js

--- story/myStory.js
+++ story/myStory.js
@@ -14,7 +14,7 @@ function initMap() {
                     }
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 3,
-      center: { lat: 37.815592, lng: 14.668300 }, 
+      center: { lat: 18.324079, lng: 13.393207 },
     });
     
 // The following script is taken (and modified) from: https://developers.google.com/maps/documentation/javascript/examples/polyline-simple#maps_polyline_simple-javascript
